e196c9195964bf61935ac4758a811f531c71105a
LaknaGammedda <laknaharindi5@gmail.com>
2017-05-27 11:51:26 +0530

    commit3

--- app/src/main/java/com/example/wathsala/quiz/QuestionLibrary.java
+++ app/src/main/java/com/example/wathsala/quiz/QuestionLibrary.java
@@ -7,53 +7,23 @@ package com.example.wathsala.quiz;
 public class QuestionLibrary {
 
     private String mQuestions [] = {
-            "Which part of the plant holds it in the soil?",
-            "This part of the plant absorbs energy from the sun.",
-            "This part of the plant attracts bees, butterflies and hummingbirds.",
-            "The _______ holds the plant upright."
+            "Which the following is not  part of the three-sphere model for system management",
+            "which process group most resorce an time",
+            "Which of following items is notnormaly included in the project chater",
+            "A ______ is a twmporary endevor undertaken to create a unique produt service of result "
+
 
     };
 
 
     private String mChoices [][] = {
-            {"Roots", "Stem", "Flower"},
-            {"Fruit", "Leaves", "Seeds"},
-            {"Bark", "Flower", "Roots"},
-            {"Flower", "Leaves", "Stem"}
+            {"techology", "information", "bussiness"},
+            {"intificality", "plainning", "executing"},
+            {"Grannt chart", "stakeholder signatuere", "Roots"},
+            {"program", "process", "project"}
     };
 
 
 
-    private String mCorrectAnswers[] = {"Roots", "Leaves", "Flower", "Stem"};
-
-
-
-
-    public String getQuestion(int a) {
-        String question = mQuestions[a];
-        return question;
-    }
-
-
-    public String getChoice1(int a) {
-        String choice0 = mChoices[a][0];
-        return choice0;
-    }
-
-
-    public String getChoice2(int a) {
-        String choice1 = mChoices[a][1];
-        return choice1;
-    }
-
-    public String getChoice3(int a) {
-        String choice2 = mChoices[a][2];
-        return choice2;
-    }
-
-    public String getCorrectAnswer(int a) {
-        String answer = mCorrectAnswers[a];
-        return answer;
-    }
 
 }
