6e695261034a89b171c83fa55da372b9bfc07d1a
ChathuraIshan <chathura.ishan94@gmail.com>
2017-05-27 16:22:42 +0530

    commit one

--- app/src/main/java/com/example/wathsala/quiz/QuestionLibrary.java
+++ app/src/main/java/com/example/wathsala/quiz/QuestionLibrary.java
@@ -56,6 +56,4 @@ public class QuestionLibrary {
         String answer = mCorrectAnswers[a];
         return answer;
     }
-
-
 }
--- app/src/main/java/com/example/wathsala/quiz/QuizActivity.java
+++ app/src/main/java/com/example/wathsala/quiz/QuizActivity.java
@@ -1,13 +1,47 @@
 package com.example.wathsala.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
+
+    private TextView mScoreView;
+    private TextView mQuestionView;
+    private Button mButtonChoice1;
+    private Button mButtonChoice2;
+    private Button mButtonChoice3;
+
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestionView = (TextView)findViewById(R.id.question);
+        mButtonChoice1 = (Button)findViewById(R.id.choice1);
+        mButtonChoice2 = (Button)findViewById(R.id.choice2);
+        mButtonChoice3 = (Button)findViewById(R.id.choice3);
+
+    }
+
+
+    public void onClick(View view){
+        Button answer = (Button) view;
+        if (answer.getText() == mAnswer){
+            mScore = mScore +1;
+            Toast.makeText(QuizActivity.this,"correct",Toast.LENGTH_SHORT).show();
+        }else Toast.makeText(QuizActivity.this,"wrong",Toast.LENGTH_SHORT).show();
+
     }
 }
